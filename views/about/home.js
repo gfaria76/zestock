e12d411e1460092e2555e9ac928bd892a28c75c4
gfaria76 <gedsonfaria@gmail.com>
2017-09-04 14:23:48 -0400

    Versão 2 - Teste Login 1/senha

--- views/about/home.js
+++ views/about/home.js
@@ -9,23 +9,23 @@ myAngular
     }])
     .controller('homeCtrl', function ($scope) {
         $scope.message = 'Zéstock - Constole de Estoque do Zé'
-        var settings = {
-            "async": true,
-            "crossDomain": true,
-            "url": "https://estagio.ufms.br/wp-login.php",
-            "method": "POST",
-            "headers": {
-                "content-type": "application/x-www-form-urlencoded",
-                "cache-control": "no-cache",
-                "postman-token": "6bd3c31e-3d8a-2269-f98a-653de3debac4"
-            },
-            "data": {
-                "log": "gedson.faria",
-                "pwd": "qazmko22."
-            }
-        }
-
-        $.ajax(settings).done(function (response) {
-            console.log(response);
-        });
+        // var settings = {
+        //     "async": true,
+        //     "crossDomain": true,
+        //     "url": "https://estagio.ufms.br/wp-login.php",
+        //     "method": "POST",
+        //     "headers": {
+        //         "content-type": "application/x-www-form-urlencoded",
+        //         "cache-control": "no-cache",
+        //         "postman-token": "6bd3c31e-3d8a-2269-f98a-653de3debac4"
+        //     },
+        //     "data": {
+        //         "log": "gedson.faria",
+        //         "pwd": ""
+        //     }
+        // }
+        //
+        // $.ajax(settings).done(function (response) {
+        //     console.log(response);
+        // });
     });
